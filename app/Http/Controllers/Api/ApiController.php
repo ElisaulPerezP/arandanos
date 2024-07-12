778fb97603e8985130056290d5502414200e07bc
ElisaulPerezP <elisaul.perez@hotmail.com>
2024-07-11 22:38:05 -0500

    feat: api controller for ardware control

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -4,119 +4,170 @@ namespace App\Http\Controllers\Api;
 
 use App\Events\CultivoInactivo;
 use App\Models\Cultivo;
+use App\Models\S1;
+use App\Models\S2;
+use App\Models\S3;
+use App\Models\S4;
+use App\Models\S5;
+use App\Models\EstadoSistema;
+use App\Models\ComandoHardware;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-
 class ApiController extends Controller
 {
-
     public function reportStop()
     {
-        event(new CultivoInactivo());
-        return response()->json(['message' => 'Parada reportada exitosamente, evento emitido'], 200);
+
+            event(new CultivoInactivo());
+            return response()->json(['message' => 'Parada reportada exitosamente, evento emitido'], 200);
+
     }
 
     public function getTanquesCommand()
     {
-        // Lógica para obtener comandos de selección
-        return response()->json([
-            'actions' => [
-                'on:valvula1',
-                'off:valvula2'
-            ]
-        ], 200);
+        // Obtener el comando actual para el sistema s1, esta tabla solo tiene una entrada
+        $estado = EstadoSistema::first();
+        $comando = ComandoHardware::find($estado->s1_id);
+
+        if ($comando) {
+            return response()->json(['actions' => json_decode($comando->comando)], 200);
+        }
+
+        return response()->json(['message' => 'Comando no encontrado'], 404);
     }
 
     public function reportTanquesState(Request $request)
     {
-        // Lógica para manejar el reporte de estado
+        // Actualizar el estado del sistema s1
+        $estado = S1::find(1);
+        $estado->update($request->all());
+
         return response()->json(['message' => 'Estado reportado exitosamente'], 200);
     }
 
     public function reportTanquesShutdown(Request $request)
     {
-        // Lógica para manejar el reporte de apagado
+        // Actualizar el estado del sistema s1 a inactivo
+        $estado = S1::find(1);
+        $estado->estado = false;
+        $estado->save();
+
         return response()->json(['message' => 'Apagado con exito'], 200);
     }
 
-
     public function getSelectorCommand()
     {
-        // Lógica para obtener comandos de selección
-        return response()->json([
-            'actions' => [
-                'on:valvula1',
-                'off:valvula2'
-            ]
-        ], 200);
+        // Obtener el comando actual para el sistema s2
+        $estado = EstadoSistema::first();
+        $comando = ComandoHardware::find($estado->s2_id);
+
+        if ($comando) {
+            return response()->json(['actions' => json_decode($comando->comando)], 200);
+        }
+
+        return response()->json(['message' => 'Comando no encontrado'], 404);
     }
 
     public function reportState(Request $request)
     {
-        // Lógica para manejar el reporte de estado
+        // Actualizar el estado del sistema s2
+        $estado = S2::find(1);
+        $estado->update($request->all());
+
         return response()->json(['message' => 'Estado reportado exitosamente'], 200);
     }
 
     public function reportShutdown(Request $request)
     {
-        // Lógica para manejar el reporte de apagado
+        // Actualizar el estado del sistema s2 a inactivo
+        $estado = S2::find(1);
+        $estado->estado = false;
+        $estado->save();
+
         return response()->json(['message' => 'Apagado con exito'], 200);
     }
+
     public function getImpulsoresCommand()
     {
-        // Lógica para obtener comandos de selección
-        return response()->json([
-            'actions' => [
-                'on:bomba1',
-                'off:bomba2'
-            ]
-        ], 200);
+        // Obtener el comando actual para el sistema s3
+        $estado = EstadoSistema::first();
+        $comando = ComandoHardware::find($estado->s3_id);
+
+        if ($comando) {
+            return response()->json(['actions' => json_decode($comando->comando)], 200);
+        }
+
+        return response()->json(['message' => 'Comando no encontrado'], 404);
     }
 
     public function reportImpulsoresState(Request $request)
     {
-        // Lógica para manejar el reporte de estado
+        // Actualizar el estado del sistema s3
+        $estado = S3::find(1);
+        $estado->update($request->all());
+
         return response()->json(['message' => 'Estado reportado exitosamente'], 200);
     }
 
     public function reportImpulsoresShutdown(Request $request)
     {
-        // Lógica para manejar el reporte de apagado
+        // Actualizar el estado del sistema s3 a inactivo
+        $estado = S3::find(1);
+        $estado->estado = false;
+        $estado->save();
+
         return response()->json(['message' => 'Apagado con exito'], 200);
     }
+
     public function getInyectoresCommand()
     {
-        // Lógica para obtener comandos de selección
-        return response()->json([
-            'actions' => [
-                'on:bomba1, 18',
-                'off:bomba2'
-            ]
-        ], 200);
+        // Obtener el comando actual para el sistema s4
+        $estado = EstadoSistema::first();
+        $comando = ComandoHardware::find($estado->s4_id);
+
+        if ($comando) {
+            return response()->json(['actions' => json_decode($comando->comando)], 200);
+        }
+
+        return response()->json(['message' => 'Comando no encontrado'], 404);
     }
-    
+
     public function reportInyectoresState(Request $request)
     {
-        // Lógica para manejar el reporte de estado
+        // Actualizar el estado del sistema s4
+        $estado = S4::find(1);
+        $estado->update($request->all());
+
         return response()->json(['message' => 'Estado reportado exitosamente'], 200);
     }
-    
+
     public function reportInyectoresShutdown(Request $request)
     {
-        // Lógica para manejar el reporte de apagado
+        // Actualizar el estado del sistema s4 a inactivo
+        $estado = S4::find(1);
+        $estado->estado = false;
+        $estado->save();
+
         return response()->json(['message' => 'Apagado con exito'], 200);
     }
+
     public function reportFlujoConteo(Request $request)
     {
-        // Lógica para manejar el reporte de conteo
+        // Actualizar el conteo de flujo para el sistema s5
+        $estado = S5::find(1);
+        $estado->update($request->all());
+
         return response()->json(['message' => 'Conteo reportado exitosamente'], 200);
     }
 
     public function reportFlujoApagado(Request $request)
     {
-        // Lógica para manejar el reporte de apagado
+        // Actualizar el estado del sistema s5 a inactivo
+        $estado = S5::find(1);
+        $estado->estado = false;
+        $estado->save();
+
         return response()->json(['message' => 'Apagado con exito'], 200);
     }
-
 }
