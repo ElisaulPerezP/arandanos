a5b8895f2241dcb777b7e71e867d3a226d45630e
ElisaulPerezP <elisaul.perez@hotmail.com>
2024-07-25 13:18:36 -0500

    fishing error on start command restart listener

--- app/Listeners/RestartEventListener.php
+++ app/Listeners/RestartEventListener.php
@@ -4,6 +4,8 @@ namespace App\Listeners;
 
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use App\Events\StopSystem;
+use App\Events\InicioDeAplicacion;
 
 class RestartEventListener
 {
@@ -20,6 +22,12 @@ class RestartEventListener
      */
     public function handle(object $event): void
     {
-        //
+         // Instanciar y llamar al listener StopSystemListener
+         $stopListener = new StopSystemListener();
+         $stopListener->handle(new StopSystem());
+ 
+         // Instanciar y llamar al listener IniciarAplicacionListener
+         $startListener = new IniciarAplicacionListener();
+         $startListener->handle(new InicioDeAplicacion());
     }
 }
