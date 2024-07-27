4b08c7a790012d86d1e0fe5b4b1770b8029385db
ElisaulPerezP <elisaul.perez@hotmail.com>
2024-07-27 18:29:34 -0500

    fix: give comando_id to s2 on reportState method in apiController

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -160,7 +160,7 @@ class ApiController extends Controller
         // Crear una nueva entrada s2 con la información proporcionada en el request y el comando del antecesor
         $s2Nueva = S2::create(array_merge(
             $request->all(),
-            ['comando' => $s2Actual ? $s2Actual->comando : null]
+            ['comando_id' => $s2Actual ? $s2Actual->comando_id : null]
         ));
 
         // Actualizar el EstadoSistema con la nueva entrada s2
