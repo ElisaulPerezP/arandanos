d19ead8d18a92c48d203f0aa493a60ac49e45d52
ElisaulPerezP <elisaul.perez@hotmail.com>
2024-07-28 13:08:33 -0500

    fix: commando description

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -259,7 +259,7 @@ class ApiController extends Controller
         // Verificar si existe el estado y la relación s4
         if ($estado && $estado->s4) {
             // Obtener el comando desde la relación s4
-            $comando = $estado->s4->comando;
+            $comando = $estado->s4->comando->descripcion;
 
             // Retornar el comando si existe
             if ($comando) {
