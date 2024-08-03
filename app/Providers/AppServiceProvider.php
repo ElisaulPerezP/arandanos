89cd001175a2d2fef0702563aa5736ca036a4fa3
ElisaulPerezP <elisaul.perez@hotmail.com>
2024-08-02 23:11:55 -0500

    feat: database configuration rollingback

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,22 +17,8 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot()
+    public function boot(): void
     {
-        if (config('database.default') === 'sqlite' && file_exists(config('database.connections.sqlite.database'))) {
-            DB::connection()->getPdo()->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-            
-            // Ejecutar las PRAGMAs
-            DB::connection('sqlite')->getPdo()->exec('PRAGMA synchronous = NORMAL;');
-            DB::connection('sqlite')->getPdo()->exec('PRAGMA journal_mode = WAL;');
-            DB::connection('sqlite')->getPdo()->exec('PRAGMA cache_size = 10000;');
-            DB::connection('sqlite')->getPdo()->exec('PRAGMA locking_mode = EXCLUSIVE;');
-            DB::connection('sqlite')->getPdo()->exec('PRAGMA temp_store = MEMORY;');
-            DB::connection('sqlite')->getPdo()->exec('PRAGMA mmap_size = 268435456;');
-            DB::connection('sqlite')->getPdo()->exec('PRAGMA busy_timeout = 30000;'); // 30 segundos de tiempo de espera
-        } else {
-            // Puedes lanzar una excepción o simplemente ignorar si el archivo no existe
-            // throw new \Exception('Database file does not exist.');
-        }
+        
     }
 }
