af4bd1f7299b4425938f4372bf534d5320012cbb
ElisaulPerezP <elisaul.perez@hotmail.com>
2024-08-02 22:56:25 -0500

    feat: database timeout seting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,16 +18,22 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
-         // Registrar el evento de conexión para SQLite
-         DB::connection()->getPdo()->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-
-         DB::connection('sqlite')->getPdo()->exec('PRAGMA synchronous = NORMAL;');
-         DB::connection('sqlite')->getPdo()->exec('PRAGMA journal_mode = WAL;');
-         DB::connection('sqlite')->getPdo()->exec('PRAGMA cache_size = 10000;');
-         DB::connection('sqlite')->getPdo()->exec('PRAGMA locking_mode = EXCLUSIVE;');
-         DB::connection('sqlite')->getPdo()->exec('PRAGMA temp_store = MEMORY;');
-         DB::connection('sqlite')->getPdo()->exec('PRAGMA mmap_size = 268435456;');
+        if (config('database.default') === 'sqlite' && file_exists(config('database.connections.sqlite.database'))) {
+            DB::connection()->getPdo()->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            
+            // Ejecutar las PRAGMAs
+            DB::connection('sqlite')->getPdo()->exec('PRAGMA synchronous = NORMAL;');
+            DB::connection('sqlite')->getPdo()->exec('PRAGMA journal_mode = WAL;');
+            DB::connection('sqlite')->getPdo()->exec('PRAGMA cache_size = 10000;');
+            DB::connection('sqlite')->getPdo()->exec('PRAGMA locking_mode = EXCLUSIVE;');
+            DB::connection('sqlite')->getPdo()->exec('PRAGMA temp_store = MEMORY;');
+            DB::connection('sqlite')->getPdo()->exec('PRAGMA mmap_size = 268435456;');
+            DB::connection('sqlite')->getPdo()->exec('PRAGMA busy_timeout = 30000;'); // 30 segundos de tiempo de espera
+        } else {
+            // Puedes lanzar una excepción o simplemente ignorar si el archivo no existe
+            // throw new \Exception('Database file does not exist.');
+        }
     }
 }
