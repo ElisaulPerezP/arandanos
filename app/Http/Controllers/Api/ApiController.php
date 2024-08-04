0cba6029181f0099237ab06ab854e0ee4cd0bc82
ElisaulPerezP <elisaul.perez@hotmail.com>
2024-08-04 11:59:59 -0500

    fix: fix

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -166,7 +166,7 @@ class ApiController extends Controller
         // Crear una nueva entrada s2 con la información proporcionada en el request y el comando del antecesor
         $s2Nueva = S2::create(array_merge(
             $request->all(),
-            ['comando_id' => $s2Actual ? $s2Actual->comando_id : null]
+            ['comando_id' => $s2Actual ? $s2Actual->comando_id : "SIN_ESTADO"]
         ));
 
         // Actualizar el EstadoSistema con la nueva entrada s2
