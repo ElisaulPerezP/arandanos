ed0d91351c6bbe426d515000bd78339f43e6e7c4
ElisaulPerezP <elisaul.perez@hotmail.com>
2024-08-05 13:50:50 -0500

    fix: implement error handling timeout

--- pythonScripts/stopManual.py
+++ pythonScripts/stopManual.py
@@ -8,6 +8,9 @@ import argparse
 import signal
 from threading import Thread
 
+# Configuración del tiempo de espera en segundos
+TIMEOUT = 2  # Tiempo de espera total de 2 segundos
+
 # Funciones para manipular GPIO
 def is_exported(pin):
     """Verificar si un pin GPIO ya está exportado."""
@@ -70,24 +73,28 @@ def read_pin_value(pin, api_error_url):
         return None
 
 # Funciones para interactuar con la API
-def report_stop(url, value):
+def report_stop(api_error_url, value):
     payload = {'estado': 'Parada activada', 'sensor3': value}
     try:
-        response = requests.post(url, json=payload)
+        response = requests.post(api_error_url, json=payload, timeout=TIMEOUT)
         if response.status_code == 200:
             print(f"Parada reportada exitosamente.")
         else:
             report_error(api_error_url, f"Error al reportar la parada: {response.status_code}")
-    except Exception as e:
+    except requests.Timeout:
+        report_error(api_error_url, "Timeout al reportar la parada.")
+    except requests.RequestException as e:
         report_error(api_error_url, f"Excepción al reportar la parada: {e}")
 
 def report_error(url, error_message):
     payload = {'error': error_message}
     try:
-        response = requests.post(url, json=payload)
+        response = requests.post(url, json=payload, timeout=TIMEOUT)
         if response.status_code != 200:
             print(f"Error al reportar el error: {response.status_code}")
-    except Exception as e:
+    except requests.Timeout:
+        print("Timeout al reportar el error.")
+    except requests.RequestException as e:
         print(f"Excepción al reportar el error: {e}")
 
 def load_first_pin_from_file(filename):
@@ -121,10 +128,17 @@ def main(input_file, stop_url, api_error_url):
             events = poller.poll(1000)  # Esperar hasta 1 segundo por un evento
             if events:
                 os.lseek(value_fd, 0, os.SEEK_SET)  # Resetear el puntero del archivo al inicio
-                value = os.read(value_fd, 1024).strip()  # Leer el valor
-                if value == b'1':
-                    report_stop(stop_url, value.decode())
-                    stop_threads = True
+                try:
+                    value = os.read(value_fd, 1024).strip()  # Leer el valor
+                    if value == b'1':
+                        report_stop(stop_url, value.decode())
+                        stop_threads = True
+                except OSError as e:
+                    if e.errno == 19:
+                        print("Dispositivo no disponible, probablemente desexportado.")
+                        break
+                    else:
+                        raise
 
         os.close(value_fd)
 
