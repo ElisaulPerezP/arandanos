503bc2cbef167435b2b811a5c1dff631dc756252
ElisaulPerezP <elisaul.perez@hotmail.com>
2024-08-07 20:47:53 -0500

    feat: EstadoSistema uuid

--- app/Models/EstadoSistema.php
+++ app/Models/EstadoSistema.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class EstadoSistema extends Model
 {
@@ -20,33 +21,44 @@ class EstadoSistema extends Model
         's5_id',
     ];
 
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (!$model->getKey()) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+
     public function s0()
     {
-        return $this->belongsTo(S0::class);
+        return $this->belongsTo(S0::class, 's0_id', 'id');
     }
 
     public function s1()
     {
-        return $this->belongsTo(S1::class);
+        return $this->belongsTo(S1::class, 's1_id', 'id');
     }
 
     public function s2()
     {
-        return $this->belongsTo(S2::class);
+        return $this->belongsTo(S2::class, 's2_id', 'id');
     }
 
     public function s3()
     {
-        return $this->belongsTo(S3::class);
+        return $this->belongsTo(S3::class, 's3_id', 'id');
     }
 
     public function s4()
     {
-        return $this->belongsTo(S4::class);
+        return $this->belongsTo(S4::class, 's4_id', 'id');
     }
 
     public function s5()
     {
-        return $this->belongsTo(S5::class);
+        return $this->belongsTo(S5::class, 's5_id', 'id');
     }
 }
