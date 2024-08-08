3a6cfeeff8f5b8ed5c965415287fd52ab0de2dfe
ElisaulPerezP <elisaul.perez@hotmail.com>
2024-08-07 20:31:35 -0500

    feat: s1 uuid

--- app/Models/S1.php
+++ app/Models/S1.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class S1 extends Model
 {
@@ -12,12 +13,24 @@ class S1 extends Model
     protected $table = 's1';
 
     protected $fillable = [
+        'id',
         'estado',
         'comando_id',
         'sensor1',
         'sensor2',
         'valvula14',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (!$model->getKey()) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
 
     public function comando()
     {
