33d78c20dac5ae6444b32b45f5435c4f4500cdbf
ElisaulPerezP <elisaul.perez@hotmail.com>
2024-08-07 20:33:18 -0500

    feat: s3 uuid

--- app/Models/S3.php
+++ app/Models/S3.php
@@ -3,6 +3,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class S3 extends Model
 {
@@ -11,11 +12,23 @@ class S3 extends Model
     protected $table = 's3';
 
     protected $fillable = [
+        'id',
         'estado',
         'comando_id',
         'pump1',
         'pump2',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (!$model->getKey()) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
 
     public function comando()
     {
