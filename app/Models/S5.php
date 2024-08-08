4d003080b2a87e58b575128ab4548edc5c65ad69
ElisaulPerezP <elisaul.perez@hotmail.com>
2024-08-07 20:35:07 -0500

    feat: s5 uuid

--- app/Models/S5.php
+++ app/Models/S5.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class S5 extends Model
 {
@@ -12,11 +13,23 @@ class S5 extends Model
     protected $table = 's5';
 
     protected $fillable = [
+        'id',
         'estado',
         'comando_id',
         'flux1',
         'flux2',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (!$model->getKey()) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
 
     public function comando()
     {
