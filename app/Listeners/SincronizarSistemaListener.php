986bed18a896bf9f5a97a71240d31bf8cde6bb15
ElisaulPerezP <elisaul.perez@hotmail.com>
2024-08-09 01:05:03 -0500

    fix: fix

--- app/Listeners/SincronizarSistemaListener.php
+++ app/Listeners/SincronizarSistemaListener.php
@@ -99,7 +99,7 @@ class SincronizarSistemaListener implements ShouldQueue
         }
 
         // Enviar estado actual
-        $estadoActualResponse = Http::withToken($token)->post("$baseUrl/api/estado/reportar", $event->estadoActual);
+        $estadoActualResponse = Http::withToken($token)->post("$baseUrl/api/estado/reportar", $event->estadoActual["estado_id"]);
 
         if ($estadoActualResponse->failed()) {
             Log::error('Failed to report estado actual', [
