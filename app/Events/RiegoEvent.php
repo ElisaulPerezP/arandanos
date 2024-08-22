9cb74f492e4f8926fe62405715f46e722d247ee9
ElisaulPerezP <elisaul.perez@hotmail.com>
2024-08-21 23:27:56 -0500

    fix: array in riego event

--- app/Events/RiegoEvent.php
+++ app/Events/RiegoEvent.php
@@ -18,7 +18,7 @@ class RiegoEvent
     public function __construct($programacion)
     {
         // Almacenar $programacion como un array para mantener flexibilidad
-        $this->programacion = $programacion->toArray();
+        $this->programacion = $programacion;
     }
 
     /**
